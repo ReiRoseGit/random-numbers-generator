2ade538e411195082aec663fb48edc0593b8284c
ReiRoseGit <rei.rose.it@mail.ru>
2022-08-24 09:15:12 +0300

    Переделана сборка webpack под production и development

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,77 @@
 const path = require('path')
-const htmlWebpackPlugin = require('html-webpack-plugin');
-const PrettierPlugin = require("prettier-webpack-plugin");
+const htmlWebpackPlugin = require('html-webpack-plugin')
+const PrettierPlugin = require('prettier-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-module.exports = {
-    entry: './src/scripts/script.ts',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    resolve: {
-        extensions: [".tsx", ".ts", ".js"]
-    },
-    module:{
-        rules: [
-            {
-                test: /\.tsx?$/,
-                loader: 'ts-loader',
+module.exports = (env) => {
+    if (env.development) {
+        console.log('Development mode!')
+        return {
+            entry: './src/scripts/script.ts',
+            output: {
+                path: path.resolve(__dirname, 'dist'),
+                filename: 'bundle.js',
             },
-            {
-                enforce: 'pre',
-                test: /\.ts?$/,
-                use: "tslint-loader"
+            resolve: {
+                extensions: ['.tsx', '.ts', '.js'],
             },
-            { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] }
-        ]
-    },
-    plugins: [
-        new htmlWebpackPlugin({template:'./src/public/index.html'}),
-        new PrettierPlugin({
-            "printWidth": 120,
-            "tabWidth": 4,
-            "useTabs": false,
-            "semi": false,
-            "encoding": "utf-8",
-            "extensions": [".ts", "js", ".css", ".less", ".html", ".xml"],
-            "singleQuote": true,
-            "arrowParens": "always",
-            "trailingComma": "es5",
-            "end-of-line": "lf"
+            module: {
+                rules: [
+                    {
+                        test: /\.tsx?$/,
+                        loader: 'ts-loader',
+                    },
+                    {
+                        enforce: 'pre',
+                        test: /\.ts?$/,
+                        use: 'tslint-loader',
+                    },
+                    { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+                ],
+            },
+            plugins: [
+                new htmlWebpackPlugin({ template: './src/public/index.html' }),
+                new PrettierPlugin({
+                    printWidth: 120,
+                    tabWidth: 4,
+                    useTabs: false,
+                    semi: false,
+                    encoding: 'utf-8',
+                    extensions: ['.ts', 'js', '.css', '.less', '.html', '.xml'],
+                    singleQuote: true,
+                    arrowParens: 'always',
+                    trailingComma: 'es5',
+                    'end-of-line': 'lf',
+                }),
+            ],
+            mode: 'development',
         }
-        )
-    ],
-    mode:'production'
+    } else if (env.production) {
+        console.log('Production mode!')
+        return {
+            entry: './src/scripts/script.ts',
+            output: {
+                path: path.resolve(__dirname, 'dist'),
+                filename: '[name].js',
+            },
+            resolve: {
+                extensions: ['.tsx', '.ts', '.js'],
+            },
+            module: {
+                rules: [
+                    {
+                        test: /\.tsx?$/,
+                        loader: 'ts-loader',
+                    },
+                    { test: /\.css$/, use: [MiniCssExtractPlugin.loader, 'css-loader'] },
+                ],
+            },
+            plugins: [
+                new htmlWebpackPlugin({ template: './src/public/index.html', filename: '[name].html', hash: true }),
+                new MiniCssExtractPlugin(),
+            ],
+            mode: 'production',
+            devtool: 'source-map',
+        }
+    }
 }
